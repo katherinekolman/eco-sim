64f78b06a99bb356ffe3bf99b409c9ecf8ee1e38
katherinekolman <katherinekolman@gmail.com>
2018-07-29 21:50:30 -0500

    added breeding

--- eco_sim/herbivore.js
+++ eco_sim/herbivore.js
@@ -16,23 +16,31 @@ class Herbivore extends Organism {
                 childDNA.push(mateDNA[i]);
             }
         }
+        
         return childDNA;
     }
 
     tryBreeding(mateDNA) {
         // needs to account fitness score, health,  randomness
-        if (((this.fitness / showBestAgent()) * (this.health / 100) * random(.3, .8)) >= .2) {
-            herbivores.push(new Herbivore(this.crossover(mateDNA), this.x, this.y));
+        let threshold = ((this.fitness / showBestAgent()) * (this.health / 100) * random(.3, .8));
 
+        if (threshold >= .78) {
+            herbivores.push(new Herbivore(this.crossover(mateDNA), this.position.x, this.position.y));
+            organisms.push(herbivores[herbivores.length - 1]);
         }
     }
 
     findMate() {
         let distance = Infinity;
         let d;
-        let closest;
+        let closest = null;
 
         for (let i = 0; i < herbivores.length; i++) {
+            // can't breed with itself
+            if (herbivores[i].num == this.num) {
+              continue;
+            }
+
             d = herbivores[i].position.dist(this.position);
 
             if (d < 10) {
@@ -50,18 +58,27 @@ class Herbivore extends Organism {
                 closest = herbivores[i];
             }
         }
+
+        // if there aren't any eligible mates nearby, find food instead
+        if (closest == null) {
+          super.findFood();
+          return;
+        }
+
         super.seek(closest);
     }
 
     update() {
         super.update();
-            //  if (this.hunger < 50 || this.health < 50) {
-            //      this.mode = animalModes.MATE;
-            //      this.findMate();
-            //  } else {
-            this.mode = animalModes.FOOD;
-            super.findFood();
-                    //  }
+
+        // should only mate when they're healthy
+        if (this.hunger < 50 && this.health > 50) {
+             this.mode = animalModes.MATE;
+             this.findMate();
+        } else {
+          this.mode = animalModes.FOOD;
+          super.findFood();
+        }
     }
 
 }
